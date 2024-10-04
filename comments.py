997b9f1baadf004b5145b2fbb34f78db82b2a011
Amir Mustafa <amirengg15@gmail.com>
2024-10-04 15:42:15 +0530

    feat: comments in python

---
+++ comments.py
@@ -0,0 +1,24 @@
+# single line comment
+name = 'Amir'
+
+print('Name: ', name)
+
+
+# multi line comment in Python | block comment
+# print('Name: ', name)
+# print('Name: ', name)
+# print('Name: ', name)
+# print('Name: ', name)
+
+# doc string
+
+"""
+print('Name: ', name)
+print('Name: ', name)
+print('Name: ', name)
+print('Name: ', name)
+"""
+
+
+
+
