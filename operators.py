6496a56bb026253ebb08091de60cd412487dbcda
Amir Mustafa <amirengg15@gmail.com>
2024-10-05 17:15:18 +0530

    feat: python arithmetic operator

---
+++ operators.py
@@ -0,0 +1,23 @@
+# Arithmetic Operator
+
+# + Addition
+print(1 + 2)
+
+# - Subtraction
+print(10 - 12)
+
+# / Division - always returns float
+print(8 / 2);
+
+# // - equivalent to Math.floor() of JS
+
+print(11 / 2)
+
+# * - multiply
+print(2 * 9)
+
+# ** - exponential
+print(2 ** 9)
+
+# % - module i.e. remainder
+print(7 % 3)
