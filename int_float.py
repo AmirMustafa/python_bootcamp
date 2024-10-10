b33af55e9dfcd54342253b455dc3ea058b30ab5a
Amir Mustafa <amirengg15@gmail.com>
2024-10-10 19:19:59 +0530

    feat: int() and float() in python

---
+++ int_float.py
@@ -0,0 +1,14 @@
+user_int = int(input("Please enter a number?"))
+
+print(user_int)
+print(type(user_int))
+
+
+user_float = float(input("Please enter a float?"))
+
+print(user_float)
+print(type(user_float))
+
+prompt = int(input("Enter a number to add to 10"))
+
+print(10 + prompt)
