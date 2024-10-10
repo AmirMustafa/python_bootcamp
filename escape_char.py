c3acce5a04b243be411033f9c36e2694eff28f9f
Amir Mustafa <amirengg15@gmail.com>
2024-10-10 18:02:19 +0530

    feat: escape chars in python

---
+++ escape_char.py
@@ -0,0 +1,19 @@
+tab = "This is \t tabbed \t string"
+
+print(tab)
+
+print("line 1\nline 2")
+
+print("\" This is string escape \"")
+
+print("Here is \\")
+
+
+# Exercise 2
+
+x = 5.04
+print(type(x))
+
+print(str(x) + ' is a float.')
+
+print("\"Hello, I\'m Amir Mustafa, it\'s nice to meet you!\"")
