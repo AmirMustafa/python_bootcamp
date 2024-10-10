4ad00af52dbedf381f51acf6be31de6adf79cf06
Amir Mustafa <amirengg15@gmail.com>
2024-10-10 18:55:40 +0530

    feat: input() in python

---
+++ input.py
@@ -0,0 +1,17 @@
+name = input("Please enter your name.")
+
+print("Your name is " + name + ".")
+print(type(name))
+
+# Eg 2
+fav_num = input("Enter you favourite no.")
+print("Your favourite number is " + fav_num)
+print(type(fav_num))
+
+
+# Eg 3
+name = input("What is your name?")
+quest = input("What is your quest?")
+favourite_color = input("What is your favourite color?")
+
+print("So your name is " + name + ", your quest is " + quest + ", and your favourite color is " + favourite_color + ".")
