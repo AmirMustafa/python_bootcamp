455c3b5bbebf83be903ebf1a38c53a1034ab263a
Amir Mustafa <amirengg15@gmail.com>
2024-10-10 17:49:06 +0530

    feat: convert to string

--- constants.py
+++ constants.py
@@ -11,4 +11,16 @@ print(type (score))
 
 print(id(score))
 
+ex_1 = str(True)
+ex_2 = str(5)
+ex_3 = str(3.21)
+
+print(ex_1)
+print(ex_2)
+print(ex_3)
+
+print(type(ex_1))
+print(type(ex_2))
+print(type(ex_3))
+
 
