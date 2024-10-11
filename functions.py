532883857a4c2ab3b28494c22a6e7f950f669704
Amir Mustafa <amirengg15@gmail.com>
2024-10-11 13:40:10 +0530

    feat: python functions

---
+++ functions.py
@@ -0,0 +1,19 @@
+def prints_four():
+    print("this")
+    print("is")
+    print("an")
+    print("eg")
+    
+    
+prints_four()
+prints_four()
+prints_four()
+prints_four()
+
+
+# Eg 2
+def function_name():
+    print(2 + 2)
+    
+
+function_name()
