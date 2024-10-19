406e2deff9a93fba20493cb6117f683a7d52348e
Amir Mustafa <amirengg15@gmail.com>
2024-10-19 19:17:58 +0530

    feat: boolean operators - or operator

--- boolean_operators.py
+++ boolean_operators.py
@@ -1,3 +1,9 @@
 print(4 > 1 and "word" == "word") # True and True
 print(0 > 1 and "word" == "word") # False and True
 print(4 > 1 and "word" == "word2") # True and False
+
+
+print(4 > 1 or "word" == "word") # True and True
+print(0 > 1 or "word" == "word") # False and True
+print(4 > 1 or "word" == "word2") # True and False
+print(0 > 1 or "word" == "word2") # False and False
