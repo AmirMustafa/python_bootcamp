1fa5b9cea8a9b86a034fd16f8404c472167ebc4e
Amir Mustafa <amirengg15@gmail.com>
2024-10-31 21:24:16 +0530

    feat: elif statement python - challenge

---
+++ elif_challenge.py
@@ -0,0 +1,22 @@
+user_num = int(input('Please enter an integer: '))
+
+if user_num == 1:
+    print("I")
+elif user_num == 2:
+    print("II")
+elif user_num == 3:
+    print("III")
+elif user_num == 4:
+    print("IV")
+elif user_num == 5:
+    print("V")
+elif user_num == 6:
+    print("VI")
+elif user_num == 7:
+    print("VII")
+elif user_num == 8:
+    print("VII")
+elif user_num == 9:
+    print("IX")
+else: 
+    print("X")
