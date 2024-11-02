0deee769ec643c8f0ca907d7173fd584b5bf19a0
Amir Mustafa <amirengg15@gmail.com>
2024-11-02 19:14:38 +0530

    feat: bool()

---
+++ boolean.py
@@ -0,0 +1,14 @@
+a = ""
+b = "ABC"
+c = 0
+d = 500
+e = 0.0
+f = 15.9
+
+
+print(bool(a))  # False
+print(bool(b))  # True
+print(bool(c))  # False
+print(bool(d))  # True
+print(bool(e))  # False
+print(bool(f))  # True
