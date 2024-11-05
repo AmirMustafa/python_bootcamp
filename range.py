ef97b4c02f64ec17b7dfbd0fc02fa5b890b9b6c3
Amir Mustafa <amirengg15@gmail.com>
2024-11-05 19:09:29 +0530

    feat: range() - 1, 2, 3, decreasing loop

--- range.py
+++ range.py
@@ -1,10 +1,10 @@
 # range with 1 argument
-# one_input = range(5)
+one_input = range(5)
 
-# for num in one_input:
-#     print(num)
+for num in one_input:
+    print(num)
     
-# print(type(one_input))
+print(type(one_input))
 
 # range with 2 arguments
 two_inputs = range(5, 10)
@@ -13,3 +13,21 @@ for num in two_inputs:
     print(num)
 
 print(type(two_inputs))
+
+# range with 3 arguments
+three_inputs = range(5, 15, 3)
+
+for num in three_inputs:
+    print(num)
+
+print(type(three_inputs))
+
+# decrementing loop
+three_inputs = range(15, 1, -3)
+
+for num in three_inputs:
+    print(num)
+
+print(type(three_inputs))
+
+    
