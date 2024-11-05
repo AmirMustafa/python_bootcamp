8ecc6884da71ca7193f7a03995771876def8704e
Amir Mustafa <amirengg15@gmail.com>
2024-11-05 18:58:57 +0530

    feat: range() with two arguments

--- range.py
+++ range.py
@@ -1,7 +1,15 @@
 # range with 1 argument
-one_input = range(5)
+# one_input = range(5)
 
-for num in one_input:
-    print(num)
+# for num in one_input:
+#     print(num)
     
-print(type(one_input))
+# print(type(one_input))
+
+# range with 2 arguments
+two_inputs = range(5, 10)
+
+for num in two_inputs:
+    print(num)
+
+print(type(two_inputs))
