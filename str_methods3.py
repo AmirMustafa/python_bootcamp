07503aed1c9d7cff32330b0dc45164969161cf75
Amir Mustafa <amirengg15@gmail.com>
2024-11-08 19:04:48 +0530

    feat: isspace()

--- str_methods3.py
+++ str_methods3.py
@@ -1,6 +1,15 @@
-print("Batman".isalpha())
-print("Batman123".isalpha())
+# print("Batman".isalpha())
+# print("Batman123".isalpha())
 
-print("Batman123".isalnum())
-print("Batman".isalnum())
-print("123".isalnum())
+# print("Batman123".isalnum())
+# print("Batman".isalnum())
+# print("123".isalnum())
+
+# print("123".isdecimal())
+# print("123abc".isdecimal())
+# print("3.14".isdecimal())
+
+print("           ".isspace())
+print("    ".isspace())
+print("not just spaces".isspace())
+print("not just spaces"[3].isspace())
