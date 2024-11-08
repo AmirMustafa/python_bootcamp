0f0bf492cb11788d152acfe0adc973dc569f108c
Amir Mustafa <amirengg15@gmail.com>
2024-11-08 18:43:08 +0530

    feat: strings methods - isupper(), islower()

---
+++ str_methods2.py
@@ -0,0 +1,7 @@
+print("Mixed Case".isupper())
+print("ALL CAPITAL CASE".isupper())
+
+print("ALL UPPER".islower())
+print("all lower case".islower())
+print("Mixed case".islower())
+print("ABC".upper().isupper())
