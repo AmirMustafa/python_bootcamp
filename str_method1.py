2f6c6a36e60fa52b9e42aba884a826b542959fb0
Amir Mustafa <amirengg15@gmail.com>
2024-11-08 18:30:18 +0530

    feat: upper(), lower() str methods

---
+++ str_method1.py
@@ -0,0 +1,12 @@
+# convert to upper case
+all_low = "all are small letters here"
+print(all_low.upper())
+
+# print(all_low)
+
+all_low = all_low.upper()
+print(all_low)
+
+# convert to lower case
+all_upp = "ALL LETTERS ARE CAPITAL"
+print(all_upp.lower())
